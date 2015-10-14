16a3069889125ff21ba44317a41a94d7b33dda76
Jiajun Zheng <359191701@qq.com>
2015-10-14 17:24:27 +0800

    简单的注释添加

--- ZJTableView/ZJTableView/ViewController.m
+++ ZJTableView/ZJTableView/ViewController.m
@@ -24,16 +24,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    // 初始化视图
     [self.view addSubview:self.tableView];
     [self.tableView addGestureRecognizer:self.pan];
-    
-    [self.pan addObserver:self forKeyPath:@"state" options:NSKeyValueObservingOptionNew context:NULL];
-    
     [self.tableView addSubview:self.cellRed];
     [self.tableView addSubview:self.cellBlue];
+    // 监听手势变化
+    [self.pan addObserver:self forKeyPath:@"state" options:NSKeyValueObservingOptionNew context:NULL];
 }
 -(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context{
     NSInteger new = [change[@"new"] integerValue];
+    // 点击判断
     if (new == UIGestureRecognizerStateFailed) {
         if ([self.tableView.hitCell isKindOfClass:[ZJTableViewCell class]]) {
             self.tableView.hitCell.label.text = @"点击";
@@ -49,20 +50,25 @@
 }
 -(void)pan:(UIPanGestureRecognizer *)pan{
     if (pan.state == UIGestureRecognizerStateBegan) {
+        // 记录点击是的位子变化
         self.beganY = self.tableView.bounds.origin.y;
         self.labelRed.text = @"滚动";
         self.labelBlue.text = @"滚动";
     }else if (pan.state == UIGestureRecognizerStateChanged) {
+        // 判断变化距离
         CGFloat y = [pan translationInView:self.tableView].y;
+        // 弹簧系数
         CGFloat realY = sqrt(y) * sqrt(y / 10);
         if (y < 0) {
             realY = y;
         }
+        // 通过改变bounds改变内部视图的位子
         CGRect bounds = CGRectMake(0, self.beganY - realY, self.tableView.bounds.size.width, self.tableView.bounds.size.height);
         self.tableView.bounds = bounds;
     }else if (pan.state == UIGestureRecognizerStateEnded){
         self.labelRed.text = @"停止";
         self.labelBlue.text = @"停止";
+        // 回弹
         if (self.tableView.bounds.origin.y != 0) {
             [UIView animateKeyframesWithDuration:0.3 delay:0 options:0 animations:^{
                 self.tableView.bounds = CGRectMake(0, 0, self.tableView.bounds.size.width, self.tableView.bounds.size.height);
